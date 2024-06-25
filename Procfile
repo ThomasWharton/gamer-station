dec394e5494fa65be6c1bff6bdb6507fbefc124d
Thomas Wharton <thomaswharton7@hotmail.com>
2024-06-25 14:39:23 +0000

    Prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamer_station.wsgi:application
--- gamer_station/settings.py
+++ gamer_station/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists("env.py"):
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-thomaswhart-gamerstatio-vnozeceqzrp.ws.codeinstitute-ide.net']
 
@@ -106,11 +107,15 @@ WSGI_APPLICATION = 'gamer_station.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
