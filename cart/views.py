a99cfc92c931cf7746659217aff1c1ec4226cedc
Thomas Wharton <thomaswharton7@hotmail.com>
2024-07-16 16:22:52 +0000

    Add adjust cart view/url. Updated script in cart template

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_cart(request):
@@ -21,3 +21,19 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    print(quantity)
+    
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        del cart[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
