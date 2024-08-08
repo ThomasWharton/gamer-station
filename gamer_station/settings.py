8b351c775b729bf5040ed33e3195d26b474b4a34
Thomas Wharton <thomaswharton7@hotmail.com>
2024-08-08 14:05:46 +0000

    Add newsletter models

--- gamer_station/settings.py
+++ gamer_station/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
     'profiles',
+    'newsletter',
 
     # Other
     'crispy_forms',
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class NewsletterSubscriber(models.Model):
+    subscriber = models.EmailField(unique=True, max_length=60)
+
+    def __str__(self):
+        return self.subscriber
+
+class Newsletter(models.Model):
+    subject = models.CharField(max_length=120, default="placeholder subject")
+    content = models.TextField(default="placeholder content")
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Newsletter {self.subject} sent on {self.created_on}"
+
+    def get_subscribers(self):
+        return NewsletterSubscriber.objects.all()
