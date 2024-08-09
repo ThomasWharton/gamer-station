977988aaaf8449a664bc2bdd9536743da5626f65
Thomas Wharton <thomaswharton7@hotmail.com>
2024-08-09 14:53:08 +0000

    Add functionality to send newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import NewsletterSubscriber, Newsletter
 from .forms import SubscriberForm, NewsletterForm
 
@@ -18,9 +20,36 @@ def subscribe(request):
 
     return HttpResponseRedirect('/')
 
+def send_newsletter(newsletter):
+    """Send the subscribers newsletter"""
+    subscribers = newsletter.get_subscribers()
+    subject = newsletter.subject
+    body = newsletter.content
+
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [subscribers]
+    )
 
 def newsletter(request):
-    newsletter_form = NewsletterForm()
+    if not request.user.is_superuser:
+        messages.error(request, 'Access denied. Naughty boy!')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        newsletter_form = NewsletterForm(request.POST)
+        if newsletter_form.is_valid:
+            newsletter = newsletter_form.save(commit=False)
+            send_newsletter(newsletter=newsletter)
+            newsletter.save()
+            messages.success(request, 'Newsletter sent successfully!')
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            message.error(request, 'Error sending newsletter')
+    else:
+        newsletter_form = NewsletterForm()
 
     template = "newsletter/newsletter.html"
 
@@ -29,3 +58,4 @@ def newsletter(request):
     }
 
     return render(request, template, context)
+
